b571d54c670837b7713137aea500763091913499
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-10-30 16:04:14 +0100

    Uspjesno testiranje naSlovo

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -12,19 +12,27 @@ public class Imenik {
 
         String dajBroj (String ime){
             TelefonskiBroj​ tb = imeBroj.get(ime);
-            //if(tb instanceof FiksniBroj){
             return tb.ispisi();
-       /* }
-        if(tb instanceof MobilniBroj){
-            return tb.ispisi();
-        }*/
-
         }
-/*
-    public naSlovo(char s){
 
+    public String naSlovo(char s){
+        int brojac = 1;
+        String s1 = new String();
+        for (HashMap.Entry<String, TelefonskiBroj​> entry : imeBroj.entrySet()) {
+            if(entry.getKey().charAt(0) == s){
+                s1 += String.valueOf(brojac);
+                s1 += ". ";
+                s1 += entry.getKey();
+                s1 += " - ";
+                s1 += entry.getValue().ispisi();
+                brojac++;
+
+                return s1;
+            }
+        }
+        return s1;
     }
-
+/*
     public Set<String> izGrada(FiksniBroj.Grad g){
 
     }
