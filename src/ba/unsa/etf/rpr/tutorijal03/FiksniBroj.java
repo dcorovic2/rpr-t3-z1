787161cf06bcccfdfa0c1f21e279d38997ffd6c3
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-10-30 14:18:18 +0100

    Fiksni broj i obrisan atribut TelefonskiBroj klase

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,12 +1,34 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.HashMap;
+
 public class FiksniBroj extends TelefonskiBroj​ {
-    enum Grad {SARAJEVO, TUZLA, ZENICA, BIHAC, ORASJE, GORAZDE, TRAVNIK, MOSTAR, SIROKIBRIJEG, LIVNO, BRCKO};
+    public String mjesto, broj;
+    public HashMap<Grad, String> vrijednost = new HashMap<>();
+    enum Grad {SARAJEVO, TUZLA, ZENICA, BIHAC, ORASJE, GORAZDE, TRAVNIK, MOSTAR, SIROKIBRIJEG, LIVNO, BRCKO}
+
     FiksniBroj(Grad grad, String broj){
-        super(broj);
+        vrijednost.put(Grad.SARAJEVO, "033");
+        vrijednost.put(Grad.TUZLA, "035");
+        vrijednost.put(Grad.ZENICA, "032");
+        vrijednost.put(Grad.BIHAC, "037");
+        vrijednost.put(Grad.ORASJE, "031");
+        vrijednost.put(Grad.GORAZDE, "038");
+        vrijednost.put(Grad.TRAVNIK, "030");
+        vrijednost.put(Grad.MOSTAR, "036");
+        vrijednost.put(Grad.SIROKIBRIJEG, "039");
+        vrijednost.put(Grad.LIVNO, "034");
+        vrijednost.put(Grad.BRCKO, "049");
+        mjesto = grad.name();
+        this.broj = broj;
+
     }
     public String ispisi(){
-        System.out.println();
+        String s = new String();
+        s += vrijednost.get(mjesto);
+        s += "/";
+        s += broj;
+        return s;
     }
 
     public int hashCode(){
--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj​.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj​.java
@@ -1,11 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public abstract class TelefonskiBroj​ {
-    protected String broj;
-    TelefonskiBroj​(String broj){
-        this.broj = broj;
-    }
-
     public abstract String ispisi();
     public abstract int hashCode();
 }
