78f9403679f3a1117355b720594206cf351439d6
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-10-30 14:39:16 +0100

    Klasa MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,7 +1,26 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MobilniBroj extends TelefonskiBroj​ {
-    MobilniBroj(int mobilnaMreza, String broj){
+import java.util.HashMap;
 
+public class MobilniBroj extends TelefonskiBroj​ {
+    private int mobilnaMreza;
+    private String broj;
+
+    MobilniBroj(int mobilnaMreza, String broj){
+        this.mobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
+
+    public String ispisi(){
+        String s = new String();
+        s += "0";
+        s += String.valueOf(mobilnaMreza);
+        s += "/";
+        s += broj;
+        return s;
+    }
+
+    public int hashCode(){
+        return TelefonskiBroj​.class.hashCode();
     }
 }
