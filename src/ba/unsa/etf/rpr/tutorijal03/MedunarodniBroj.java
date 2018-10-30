07cbd2532020d15a28d76be8d7b10bd444182fd8
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-10-30 14:42:08 +0100

    Proba MedunardniBroj klasa

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -1,7 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class MedunarodniBroj extends TelefonskiBroj​ {
-    MedunarodniBroj(String drzava, String broj){
+    private String drzava, broj;
 
+    MedunarodniBroj(String drzava, String broj){
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
+    public String ispisi(){
+        String s = new String();
+        s += drzava;
+        s += broj;
+
+        return s;
+    }
+
+    public int hashCode(){
+        return TelefonskiBroj​.class.hashCode();
     }
 }
