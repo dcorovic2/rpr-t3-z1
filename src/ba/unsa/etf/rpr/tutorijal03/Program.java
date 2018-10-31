f92e7041322262682e670e747882d9bdf937db9e
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-10-31 17:17:53 +0100

    Finalno.

--- src/ba/unsa/etf/rpr/tutorijal03/Program.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.Scanner;
 import java.util.Set;
 
 import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad.BIHAC;
@@ -8,33 +9,31 @@ import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad.SARAJEVO;
 public class Program {
 
     public static void main(String[] args) {
-		/*Imenik imenik = new Imenik();
-		imenik.dodaj("Dalila Corovic", new FiksniBroj(SARAJEVO, "123-123"));
-		imenik.dodaj("Dodati korisnik", new FiksniBroj(BIHAC, "525-485"));
-		imenik.dodaj("Anonimni kornsik", new MobilniBroj(62, "888-666"));
-		imenik.dodaj("Strani korisnik", new MedunarodniBroj("+1", "25 85-75-89"));
-
-		String slovo = imenik.naSlovo('D');
-		System.out.println(slovo);
-
-		Set<String> brojeviIzGrada = imenik.izGrada(BIHAC);
-		for(String broj: brojeviIzGrada){
-			System.out.println(broj + ",");
-		}*/
-
 		Imenik imenik = new Imenik();
-		imenik.dodaj("Ivo Ivic", new FiksniBroj(SARAJEVO, "123-456"));
-		imenik.dodaj("Sara Sarac", new FiksniBroj(SARAJEVO, "123-156"));
-		imenik.dodaj("Meho Mehic", new FiksniBroj(SARAJEVO, "123-656"));
-		imenik.dodaj("Pero Peric", new MobilniBroj(64, "987-654"));
-		imenik.dodaj("John Smith", new MedunarodniBroj("+1", "23 45-67-89"));
-		Set<TelefonskiBroj> set = imenik.izGradaBrojevi(SARAJEVO);
-		String result = "";
-		for (TelefonskiBroj broj: set) {
-			System.out.println(broj.ispisi() + ",");
+
+		Scanner unos = new Scanner(System.in);
+
+		System.out.println("1. Unos podataka");
+		System.out.println("2. Na slovo");
+		System.out.println("3. Iz grada");
+		System.out.println("4. Iz grada brojevi");
+		int pom = unos.nextInt();
+
+		if(pom == 1){
+			imenik.dodaj("Dalila Corovic", new FiksniBroj(SARAJEVO, "123-123"));
+			imenik.dodaj("Dodati korisnik", new FiksniBroj(BIHAC, "525-485"));
+			imenik.dodaj("Anonimni kornsik", new MobilniBroj(62, "888-666"));
+			imenik.dodaj("Strani korisnik", new MedunarodniBroj("+1", "25 85-75-89"));
 		}
-		//assertEquals("033/123-156,033/123-456,033/123-656,", result);
-
-
+		if(pom == 2){
+			String slovo = imenik.naSlovo('D');
+			System.out.println(slovo);
+		}
+		if(pom == 3){
+			Set<String> brojeviIzGrada = imenik.izGrada(BIHAC);
+			for(String broj: brojeviIzGrada){
+				System.out.println(broj + ",");
+			}
+		}
 	}
 }
